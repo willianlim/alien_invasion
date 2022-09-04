a427c49288ce841c5355ee70a62737bd2af190e0
Willian-code <willianrosendodelima@outlook.com>
2022-09-04 15:29:38 -0300

    storing bullets in a group

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from ship import Ship
 from settings import Settings
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -18,6 +19,7 @@ class AlienInvasion:
 		pygame.display.set_caption("Alien Invasion")
 
 		self.ship = Ship(self)
+		self.bullets = pygame.sprite.Group()
 
 
 	def run_game(self):
@@ -26,6 +28,7 @@ class AlienInvasion:
 			# Wach for keyboard and mouse events.
 			self._check_events()
 			self.ship.update()
+			self.bullets.update()
 			self._update_screen()
 
 			# Redraw the scree during each pass through the loop.
@@ -53,6 +56,8 @@ class AlienInvasion:
 			self.ship.moving_left = True
 		elif event.key == pygame.K_q:
 			sys.exit()
+		elif event.key == pygame.K_SPACE:
+			self._fire_bullet()
 
 	def _check_keyup_events(self, event):
 		"""Respond to key releases."""
@@ -61,10 +66,17 @@ class AlienInvasion:
 		elif event.key == pygame.K_LEFT:
 			self.ship.moving_left = False
 
+	def _fire_bullet(self):
+		"""Create a new bullet and add it to the bullets group."""
+		new_bullet = Bullet(self)
+		self.bullets.add(new_bullet)
+
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen."""
 		self.screen.fill(self.settings.bg_color)
 		self.ship.blitme()
+		for bullet in self.bullets.sprites():
+			bullet.draw_bullet()
 
 		pygame.display.flip()
 
